1feb9754584275c32554d5d50abd42324d125c9f
your name <phanh2412@gmail.com>
2022-05-06 14:04:06 +0700

    .

---
+++ WebShopping/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShopping.Models;
+
+namespace WebShopping.Controllers
+{
+    public class ProductController : Controller
+    {
+        Shopping_DBContext db = new Shopping_DBContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var product = db.Products.SqlQuery("Select * from ");
+            return View();
+        }
+
+    }
+}
---
+++ WebShopping/Models/Payment.cs
@@ -0,0 +1,25 @@
+namespace WebShopping.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Payment
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Payment()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int PaymentID { get; set; }
+
+        [StringLength(100)]
+        public string PaymentName { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
