abe6b9a879fcdb14b360152d0a6209d89e8a5369
Viet Anh <phamhoangviet.anh136@gmail.com>
2022-05-08 21:57:07 +0700

    Update AdminLoginRequireAttribute.cs

--- WebShopping/Filters/AdminLoginRequireAttribute.cs
+++ WebShopping/Filters/AdminLoginRequireAttribute.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebShopping.Areas.Admin.Models;
+using WebShopping.Models;
 
 namespace WebShopping.Filters
 {
@@ -15,7 +15,7 @@ namespace WebShopping.Filters
             {
 
                 if (filterContext.ActionDescriptor.IsDefined(typeof(AllowAnonymousAttribute), true)) return;
-                AdminSession adminSession = (AdminSession)filterContext.HttpContext.Session["AdminSession"];
+                quan_tri_vien adminSession = (quan_tri_vien)filterContext.HttpContext.Session["QuanTriVienSession"];
 
                 if (adminSession == null)
                 {
