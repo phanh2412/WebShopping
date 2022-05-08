44486da9da3ea8479f753f54e00802a15164ca66
Viet Anh <phamhoangviet.anh136@gmail.com>
2022-05-08 21:57:00 +0700

    Create helper.js

---
+++ WebShopping/Scripts/helper.js
@@ -0,0 +1,14 @@
+﻿var Enum = {
+    ResponseStatus: {
+        SUCCESS: 'success',
+        ERROR: 'error',
+        UNAUTHOR: 'unauthor',
+        UNAUTHEN: 'unauthen'
+    }
+}
+
+var GetObjectProperty = function (obj, prop, defaultValue = '') {
+    if (obj === '' || obj === null || typeof obj === 'undefined') return defaultValue;
+    if (obj[prop] === '' || obj[prop] === null || typeof obj[prop] === 'undefined') return defaultValue;
+    return obj[prop];
+}
