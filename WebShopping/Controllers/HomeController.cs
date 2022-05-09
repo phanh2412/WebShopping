4364a51a0cca2479743f445a0a9a50a32be051ed
your name <phanh2412@gmail.com>
2022-05-09 09:33:17 +0700

    .
    - giao diện người dùng

--- WebShopping/Controllers/HomeController.cs
+++ WebShopping/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebShopping.Models;
 
 namespace WebShopping.Controllers
 {
     public class HomeController : Controller
     {
+        TinaFlowers_DBContext db = new TinaFlowers_DBContext();
         // GET: Home
         public ActionResult Index()
         {
@@ -15,15 +17,25 @@ namespace WebShopping.Controllers
         }
 
 
-        public ActionResult About()
-        {
-            return View();
-        }
-
         public ActionResult Contact()
         {
             return View();
         }
 
+        public ActionResult HuongDanThanhToan()
+        {
+            return View();
+        }
+
+        public ActionResult CamKet()
+        {
+            return View();
+        }
+
+        public ActionResult RenderNav()
+        {
+            List<danh_muc_san_pham> listDanhMuc = db.danh_muc_san_pham.ToList();
+            return PartialView("_HeaderPartialView", listDanhMuc);
+        }
     }
 }
