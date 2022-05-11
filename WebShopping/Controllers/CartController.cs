7aff54c76884177a1b997c00a1df1d5227fc2e39
Viet Anh <42889092+vietanh136@users.noreply.github.com>
2022-05-11 12:15:43 +0700

    Anhphv (#25)
    * .
    
    * Create helper.js
    
    * Update AdminLoginRequireAttribute.cs
    
    * Update AdminSession.cs
    
    * .
    
    * Update tinaflowers.sql
    
    * .
    
    * Update WebShopping.csproj
    
    * cap nhat giao dien va du lieu trang Admin Tong Quan
    
    * them module ckeditor
    
    * .
    
    * Update WebShopping.csproj
    
    * quan ly san pham - hoan thanh
    
    * Update WebShopping.csproj
    
    * .
    
    * Delete WebShopping.csproj.FileListAbsolute.txt
    
    * .
    
    * Update tinaflowers.sql
    
    * .
    
    * .
    
    * /

--- WebShopping/Controllers/CartController.cs
+++ WebShopping/Controllers/CartController.cs
@@ -16,11 +16,40 @@ namespace WebShopping.Controllers
             khach nguoidung = (khach)HttpContext.Session["NguoiDungSession"];
             gio_hang gioHang = db.gio_hang.FirstOrDefault(x => x.KhachId == nguoidung.KhachId);
 
-            ViewBag.ListItemInCart = db.chi_tiet_gio_hang.Select(x=>new {x.ChiTietGioHangId,x.GioHangId,x.SanPhamId,x.SoLuong,x.san_pham.TenSanPham,x.san_pham.Gia,x.san_pham.AnhDaiDien }).Where(x => x.GioHangId == gioHang.GioHangId).ToList();
+            ViewBag.ListItemInCart = db.chi_tiet_gio_hang.Where(x => x.GioHangId == gioHang.GioHangId).Select(x => new CartItemVIewModel() { ChiTietGioHangId = x.ChiTietGioHangId, GioHangId = x.GioHangId, SanPhamId = x.SanPhamId, SoLuong = x.SoLuong, TenSanPham = x.san_pham.TenSanPham, Gia = x.san_pham.Gia, AnhDaiDien = x.san_pham.AnhDaiDien }).ToList();
 
             return View();
         }
+        [UserLoginRequire]
+        [HttpGet]
+        public ActionResult Checkout()
+        {
+            try
+            {
+                khach nguoidung = (khach)HttpContext.Session["NguoiDungSession"];
+                gio_hang gioHang = db.gio_hang.FirstOrDefault(x => x.KhachId == nguoidung.KhachId);
+                if (gioHang == null) return Error();
 
+                List<chi_tiet_gio_hang> lsChiTietGioHang = db.chi_tiet_gio_hang.Where(x => x.GioHangId == gioHang.GioHangId).ToList();
+                if (lsChiTietGioHang.Count <= 0) return Error("Giỏ hàng không có sản phẩm.");
+
+                don_dat_hang donDatHang = new don_dat_hang();
+                donDatHang.KhachId = nguoidung.KhachId;
+                donDatHang.TongTien = 0;
+                donDatHang.TrangThai = Constant.TrangThaiDonDatHang.CHO_XAC_NHAN;
+                donDatHang.NgayTao = DateTime.Now;
+                donDatHang.DonDatHangId = Guid.NewGuid().ToString();
+                
+                for(int index = 0;index< lsChiTietGioHang.Count; index++)
+                {
+                
+                }
+
+                return Success();
+
+            }
+            catch (Exception ex) { return Error(); }
+        }
 
 
         [HttpGet]
@@ -78,6 +107,66 @@ namespace WebShopping.Controllers
                     transaction.Commit();
                     return Success();
                 }
+            }
+            catch (Exception ex) { return Error(ex.Message); }
+        }
+
+        [HttpGet]
+        [UserLoginRequire]
+        public ActionResult DecreaseQuantity(int? chiTietId)
+        {
+            try
+            {
+                if (!chiTietId.HasValue) return Error();
+                chi_tiet_gio_hang chiTiet = db.chi_tiet_gio_hang.FirstOrDefault(x => x.ChiTietGioHangId == chiTietId.Value);
+                if (chiTiet == null) return Error();
+                san_pham sanPham = db.san_pham.FirstOrDefault(x => x.SanPhamId == chiTiet.SanPhamId);
+                if (sanPham == null) return Error();
+                gio_hang gioHang = db.gio_hang.FirstOrDefault(x => x.GioHangId == chiTiet.GioHangId);
+                if (gioHang == null) return Error();
+
+                chiTiet.SoLuong -= 1;
+                if (chiTiet.SoLuong <= 0)
+                {
+                    db.chi_tiet_gio_hang.Remove(chiTiet);
+                }
+
+                gioHang.TongSanPham -= 1;
+                gioHang.TongTien -= sanPham.Gia;
+
+                db.SaveChanges();
+                return Success();
+            }
+            catch (Exception ex) { return Error(ex.Message); }
+        }
+
+        [HttpGet]
+        [UserLoginRequire]
+        public ActionResult IncreaseQuantity(int? chiTietId)
+        {
+            try
+            {
+                if (!chiTietId.HasValue) return Error();
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    chi_tiet_gio_hang chiTiet = db.chi_tiet_gio_hang.FirstOrDefault(x => x.ChiTietGioHangId == chiTietId.Value);
+                    if (chiTiet == null) return Error();
+
+                    san_pham sanPham = db.san_pham.FirstOrDefault(x => x.SanPhamId == chiTiet.SanPhamId);
+                    if (sanPham == null) return Error();
+
+                    chiTiet.SoLuong += 1;
+
+                    gio_hang gioHang = db.gio_hang.FirstOrDefault(x => x.GioHangId == chiTiet.GioHangId);
+                    if (gioHang == null) return Error();
+                    gioHang.TongSanPham += 1;
+                    gioHang.TongTien += sanPham.Gia;
+
+                    db.SaveChanges();
+                    transaction.Commit();
+                    return Success();
+                }
+
             }
             catch (Exception ex) { return Error(ex.Message); }
         }
